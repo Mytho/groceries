0986087425f6af035335e43bb69b15337e292969
T. Zengerink <t.zengerink@gmail.com>
2014-07-08 17:41:15 +0200

    added functional animation

--- static/js/groceries/core.js
+++ static/js/groceries/core.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Groceries = angular.module('Groceries', ['ngTouch']);
+var Groceries = angular.module('Groceries', ['ngTouch', 'ngAnimate']);
 
 Groceries.config(['$interpolateProvider', function ($interpolateProvider) {
     $interpolateProvider.startSymbol('[[').endSymbol(']]');
